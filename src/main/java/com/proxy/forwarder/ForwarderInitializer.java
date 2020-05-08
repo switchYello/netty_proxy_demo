f18face177611defbc57d14e77e4649306e11ad5
switchYello <1062641125@qq.com>
2020-05-08 21:09:34 +0800

    cre

--- src/main/java/com/proxy/forwarder/ForwarderInitializer.java
+++ src/main/java/com/proxy/forwarder/ForwarderInitializer.java
@@ -6,7 +6,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.handler.logging.LoggingHandler;
 
-/**
+/*
  * 将连接全部转发到另外一个服务器:端口上
  * 这里硬编码www.bilibili.com:80做示例
  * 当然bilibili那边是不会处理我们的请求的，会返回一个403
